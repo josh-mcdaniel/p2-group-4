598b3dd2a34d430e421b0792b943f28b3b4aeea4
josh-mcdaniel <josh.mcdanielx3@gmail.com>
2022-05-11 17:05:10 -0400

    added a base login screen

--- Main/FrontEnd/codalz/src/components/Login/Login.tsx
+++ Main/FrontEnd/codalz/src/components/Login/Login.tsx
@@ -8,14 +8,16 @@ export const Login: React.FC<any> = () => {
         <div className="login">
 
         <div className="text-container">
-            <h1>Welcome to CODALZ!</h1>
-            <h3>Please Sign In Below</h3>
+            <header>
+                <h1>Welcome to CODALZ!</h1>
+                <h3>Please Sign In Below</h3>
+            </header>
 
             <div className="input-container">
-                <input type="text" name="username" placeholder="Username"/>
+                <input type="text" name="username" className='input-box' placeholder="Username"/>
             </div>
             <div className="input-container">
-                <input type="password" name="password" placeholder="Password"/>
+                <input type="password" name="password" className='input-box' placeholder="Password"/>
             </div>
 
             <button className="login-button">LOGIN</button>

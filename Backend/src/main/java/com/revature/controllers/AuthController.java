b93c35dabe4cdd9dc88030704d7c67938eb9ab33
mattydude87 <mattyd12334@yahoo.com>
2022-05-19 23:12:33 -0400

    addtl. changes to dtos and daos

--- Backend/src/main/java/com/revature/controllers/AuthController.java
+++ Backend/src/main/java/com/revature/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.revature.controllers;
 
 import com.google.gson.Gson;
 import com.revature.models.LoginDTO;
+import com.revature.models.User;
 import com.revature.services.AuthService;
 import io.javalin.http.Handler;
 

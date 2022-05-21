09da2176ab106a61d687e96ea1af7d885f3c8131
X <63634734+GreenTeaDay@users.noreply.github.com>
2022-05-21 17:35:14 -0400

    Login Page Function to be merged

---
+++ p2-react/src/components/App/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;

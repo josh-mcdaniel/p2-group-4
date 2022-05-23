776a648d8b90dd5be9680d5198eb8f9865b82ae8
X <63634734+GreenTeaDay@users.noreply.github.com>
2022-05-23 04:32:26 -0400

    Login clean up

--- p2-react/src/components/Register/Register.tsx
+++ p2-react/src/components/Register/Register.tsx
@@ -35,11 +35,11 @@ export const Register: React.FC<any> = () => {
                     <p className='form-animation'>USERNAME</p>
                     <input className='input-form-borders' type="text" name="username" id="username" onChange={handleChange} />
                 </div>
-            <div className="input-container">
+            <div className="input-container1">
                     <p className='form-animation'>PASSWORD</p>
                     <input className='input-form-borders' type="password" name="password" onChange={handleChange} />
             </div>
-            <div className='input-container'>
+            <div className='input-container2'>
                 <p className='form-animation'>EMAIL</p>
                 <input className='input-form-borders' type="email" name="email" onChange={handleChange} />
             </div>
